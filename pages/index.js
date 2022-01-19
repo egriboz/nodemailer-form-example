5d8d85c2655f979fb6506d8e8dda887707da8903
Fatih Eğriboz <fatih.egriboz@bilgi.edu.tr>
2022-01-19 14:32:09 +0300

    validate example

--- pages/index.js
+++ pages/index.js
@@ -7,39 +7,84 @@ export default function Home() {
   const [message, setMessage] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
-  const handleSubmit = (e) => {
+  //   Form validation
+  const [errors, setErrors] = useState({});
+  function validateEmail(email) {
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email.match(regexEmail)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  const handleValidation = () => {
+    let tempErrors = {};
+    let isValid = true;
+
+    if (name.length <= 0) {
+      tempErrors["name"] = true;
+      isValid = false;
+    }
+    // if (email.length <= 0) {
+    //   tempErrors["email"] = true;
+    //   isValid = false;
+    // }
+    if (!validateEmail(email)) {
+      tempErrors["email"] = true;
+      isValid = false;
+    }
+
+    if (message.length <= 0) {
+      tempErrors["message"] = true;
+      isValid = false;
+    }
+
+    setErrors({ ...tempErrors });
+    console.log("tempErrors", tempErrors);
+    console.log("errors", errors);
+    return isValid;
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log("Sending...");
+    let isValidForm = handleValidation();
 
     let data = {
       name,
       email,
       message,
     };
-
-    fetch("/api/mail", {
-      method: "POST",
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    }).then((res) => {
-      console.log("Response received");
-      if (res.status === 200) {
-        console.log("Response succeeded!");
-        setSubmitted(true);
-        setName("");
-        setEmail("");
-        setMessage("");
-      }
-    });
+    if (isValidForm) {
+      console.log("Sending...");
+      const res = await fetch("/api/mail", {
+        method: "POST",
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }).then((res) => {
+        console.log("Response received");
+        if (res.status === 200) {
+          console.log("Response succeeded!");
+          setSubmitted(true);
+          setName("");
+          setEmail("");
+          setMessage("");
+        }
+      });
+      // const { error } = await res.json();
+      // if (error) {
+      //   console.log("errorerror:", error);
+      //   return;
+      // }
+    }
   };
   return (
     <div className={styles.container}>
       <h1>Contact Form with Next.js and Nodemailer</h1>
-      <form className={styles.main}>
+      <form onSubmit={handleSubmit} className={styles.main}>
         <div className={styles.inputGroup}>
           <label htmlFor="name">Name</label>
           <input
@@ -50,6 +95,9 @@ export default function Home() {
             name="name"
             className={styles.inputField}
           />
+          {errors?.name && (
+            <p className={styles.errors}>Name cannot be empty.</p>
+          )}
         </div>
         <div className={styles.inputGroup}>
           <label htmlFor="email">Email</label>
@@ -61,6 +109,9 @@ export default function Home() {
             name="email"
             className={styles.inputField}
           />
+          {errors?.email && (
+            <p className={styles.errors}>e-mail cannot be empty.</p>
+          )}
         </div>
         <div className={styles.inputGroup}>
           <label htmlFor="message">Message</label>
@@ -72,12 +123,15 @@ export default function Home() {
             name="message"
             className={styles.inputField}
           />
+          {errors?.message && (
+            <p className={styles.errors}>Message cannot be empty.</p>
+          )}
         </div>
         <input
           type="submit"
-          onClick={(e) => {
-            handleSubmit(e);
-          }}
+          // onClick={(e) => {
+          //   handleSubmit(e);
+          // }}
         />
       </form>
     </div>

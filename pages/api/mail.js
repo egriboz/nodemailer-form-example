5e777f0b4c744be494b625054c82b6ec3ddbf73e
fatih egriboz <fatih.egriboz@bilgi.edu.tr>
2022-01-21 01:41:12 +0300

    ...

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -19,19 +19,19 @@ export default function handler(req, res) {
     text: req.body.message + " | Sent from: " + req.body.email,
     html: `<div style="border:1px solid #eceff1;padding: 30px;"><h1>Contact Form with Next.js and Nodemailer</h1><p>${req.body.message}</p><p style="padding: 30px 0 0 0;margin: 30px 0 0 0;border-top: 1px solid #eceff1;">Sent from: ${req.body.email}</p><p style="padding:30px 0 0 0;margin:30px 0 0 0;border-top:1px solid #eceff1"><img src="https://egriboz.com/raven.gif"></p></div>`,
   };
-  // transporter.sendMail(mailData, (err, info) => {
-  //   if (err) {
-  //     console.log(err);
-  //     res.send("error" + JSON.stringify(err));
-  //   } else {
-  //     console.log("mail send", info);
-  //     res.send("success");
-  //   }
-  // });
-  transporter.sendMail(mailData, function (err, info) {
-    if (err) console.log(err);
-    else console.log(info);
+  transporter.sendMail(mailData, (err, info) => {
+    if (err) {
+      console.log(err);
+      res.send("error" + JSON.stringify(err));
+    } else {
+      console.log("mail send", info);
+      res.send("success");
+    }
   });
+  // transporter.sendMail(mailData, function (err, info) {
+  //   if (err) console.log(err);
+  //   else console.log(info);
+  // });
 
   console.log(req.body);
   res.send("success");
